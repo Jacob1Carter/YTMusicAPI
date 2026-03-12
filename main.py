b5aa856a5aa6a9d46d60c1c8d3d3115eb0a41640
Jacob1Carter <jacob1carter@outlook.com>
2026-03-12 00:45:17 +0000

    Working system with fixes needed

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import av
 import numpy as np
 import soundfile as sf
 
-def run():
-    video_url = "https://www.youtube.com/watch?v=fxvkI9MTQw4"
+def single(video_url):
 
     ydl_opts = {
         'format': 'bestaudio/best',
         'keepvideo': False,
-        'outtmpl': '%(title)s.%(ext)s',
+        'outtmpl': f'webm/%(title)s.%(ext)s',
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -19,7 +18,7 @@ def run():
 
         ydl.download([video_url])
 
-    name = filename_webm.split(".")
+    name = filename_webm.split(".")[0][5:]
     container = av.open(filename_webm)
     stream = container.streams.audio[0]
 
@@ -30,9 +29,63 @@ def run():
 
     audio = np.concatenate(frames, axis=1).T
 
-    sf.write(f"{name}.wav", audio, stream.rate)
+    sf.write(f"wav/{name}.wav", audio, stream.rate)
 
     print("Download complete!")
 
+
+def playlist(playlist_url):
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        playlist_info = ydl.extract_info(playlist_url, download=False)
+
+        for entry in playlist_info["entries"]:
+            single(entry["webpage_url"])
+    
+    print("All downloads complete!")
+
+
+def main():
+    print("###############################################################")
+    print("#|/                                                         \|#")
+    print("#|                      URL ------> WAV                      |#")
+    print("#|                                                           |#")
+    print(" /                                                           \\ ")
+    print("/   single     playlist    open     clear     help    close   \\")
+    run = True
+    while run:
+        print()
+        action = input(" > ")
+        while action not in ["single", "playlist", "open", "clear", "help", "close"]:
+            print(" please select and valid action")
+            action = input(" > ")
+        if action == "single":
+            print(" enter URL:")
+            url = input(" > ")
+            single(url)
+        if action == "playlist":
+            print(" enter URL:")
+            url = input(" > ")
+            playlist(url)
+        if action == "open":
+            print("not yet implemented")
+        if action == "clear":
+            print("not yet implemented")
+        if action == "help":
+            print("not yet implemented")
+        if action == "close":
+            run = False
+            exit()
+
+
 if __name__ == '__main__':
-    run()
+    main()
+
+    #single("https://www.youtube.com/watch?v=fxvkI9MTQw4")
+    #playlist("https://www.youtube.com/playlist?list=OLAK5uy_lTNYcmbWKiwu3f5YJvNLjHUN4xe9nRknc")
